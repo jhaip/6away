42c9562905a4ee53068369fea09b808a1fa4b48c
Jacob Haip <jhaip@mit.edu>
2013-01-17 02:50:29 -0500

    sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,12 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+MyMovies::Application.configure do
+
+	config.action_mailer.delivery_method = :smtp
+	config.action_mailer.raise_delivery_errors = true
+
+end
+
 # Initialize the rails application
 Tutorial::Application.initialize!
