a6c37d52c14a814da2ba29ca11383db9dbd11f83
Jacob Haip <jhaip@mit.edu>
2013-01-21 20:18:46 -0500

    testing remember_me and no username

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # attr_accessible :title, :body
   attr_accessible :username, :email, :password, :password_confirmation
 
-  validates_uniqueness_of :username
+  validates_presence_of :email
   validates_uniqueness_of :email
   validates_length_of :password, :minimum => 3, :message => "password must be at least 3 characters long", :if => :password
   validates_confirmation_of :password, :message => "should match confirmation", :if => :password
