4a1c134038bf6fb1169159bb17dcdcb158129897
Jacob Haip <jhaip@mit.edu>
2013-01-25 19:10:38 -0500

    unique id's in database for smooth graphz

--- app/controllers/graph_controller.rb
+++ app/controllers/graph_controller.rb
@@ -69,7 +69,9 @@ class GraphController < ApplicationController
       likes << "Time alone"
     end
 
-    puts "Adding: "+full_name+", "+year+", "+major+", "+living_group+", "+athena_name
+    unique_id = (0...50).map{ ('a'..'z').to_a[rand(26)] }.join
+
+    puts "Adding: "+full_name+", "+year+", "+major+", "+living_group+", "+athena_name+", "+unique_id
     puts "likes:"
     puts likes
 
@@ -87,7 +89,8 @@ class GraphController < ApplicationController
                                        "name"=>full_name,
                                        "course"=>major,
                                        "year"=>year,
-                                       "living_group"=>living_group })
+                                       "living_group"=>living_group,
+                                       "id"=>unique_id })
     if likes.length == 0
       likes = ["empty"]
     end
@@ -101,15 +104,32 @@ class GraphController < ApplicationController
 
     @neo.create_node_index("nodes")
 
-  	me = @neo.create_node("athena" => "jhaip","name"=>"Jacob Haip","course"=>6,"year"=>2,"living_group"=>"Pi Lambda Phi","likes"=>["art","tech"])
-  	user1 = @neo.create_node("athena" => "__user1","name"=>"User One","course"=>2,"year"=>1,"living_group"=>"Next","likes"=>["art","tech"])
-  	user2 = @neo.create_node("athena" => "__user2","name"=>"User Two","course"=>2,"year"=>1,"living_group"=>"Next","likes"=>["art","tech"])
-  	user3 = @neo.create_node("athena" => "__user3","name"=>"User Three","course"=>2,"year"=>1,"living_group"=>"Next","likes"=>["art","tech"])
-  	@neo.create_relationship("lives_with",me,user1)
-  	@neo.create_relationship("lives_with",me,user2)
-  	@neo.create_relationship("hangs_with",me,user2)
-  	@neo.create_relationship("hangs_with",me,user3)
-  	@neo.create_relationship("urop",me,user3)
+    unique_id = (0...50).map{ ('a'..'z').to_a[rand(26)] }.join
+  	me = @neo.create_node("athena" => "jhaip","id"=>unique_id,"name"=>"Jacob Haip","course"=>6,"year"=>2,"living_group"=>"Pi Lambda Phi","likes"=>["art","tech"])
+    unique_id = (0...50).map{ ('a'..'z').to_a[rand(26)] }.join
+  	user1 = @neo.create_node("athena" => "__user1","id"=>unique_id,"name"=>"User One","course"=>2,"year"=>1,"living_group"=>"Next","likes"=>["art","tech"])
+  	unique_id = (0...50).map{ ('a'..'z').to_a[rand(26)] }.join
+    user2 = @neo.create_node("athena" => "__user2","id"=>unique_id,"name"=>"User Two","course"=>2,"year"=>1,"living_group"=>"Next","likes"=>["art","tech"])
+    unique_id = (0...50).map{ ('a'..'z').to_a[rand(26)] }.join
+  	user3 = @neo.create_node("athena" => "__user3","id"=>unique_id,"name"=>"User Three","course"=>2,"year"=>1,"living_group"=>"Next","likes"=>["art","tech"])
+  	
+    rel1 = @neo.create_relationship("lives_with",me,user1)
+  	rel2 = @neo.create_relationship("lives_with",me,user2)
+  	rel3 = @neo.create_relationship("hangs_with",me,user2)
+  	rel4 = @neo.create_relationship("hangs_with",me,user3)
+  	rel5 = @neo.create_relationship("urop",me,user3)
+
+    unique_id = (0...50).map{ ('a'..'z').to_a[rand(26)] }.join
+    @neo.set_relationship_properties(rel1, {"id" => unique_id})
+    unique_id = (0...50).map{ ('a'..'z').to_a[rand(26)] }.join
+    @neo.set_relationship_properties(rel2, {"id" => unique_id})
+    unique_id = (0...50).map{ ('a'..'z').to_a[rand(26)] }.join
+    @neo.set_relationship_properties(rel3, {"id" => unique_id})
+    unique_id = (0...50).map{ ('a'..'z').to_a[rand(26)] }.join
+    @neo.set_relationship_properties(rel4, {"id" => unique_id})
+    unique_id = (0...50).map{ ('a'..'z').to_a[rand(26)] }.join
+    @neo.set_relationship_properties(rel5, {"id" => unique_id})
+
     @neo.add_node_to_index("nodes", "name", "jhaip", me)
     @neo.add_node_to_index("nodes", "name", "__user1", user1)
     @neo.add_node_to_index("nodes", "name", "__user2", user2)
@@ -123,31 +143,44 @@ class GraphController < ApplicationController
     # should be passed params: name, type, id, and parent name
   	@neo = Neography::Rest.new(ENV['NEO4J_URL'] || "http://localhost:7474")
     if params[:type] == "person"
-    	query = @neo.execute_query("START n=node(*) WHERE n.athena ='#{params[:name]}' RETURN n.name, n.course, n.year, n.living_group, n.likes;")["data"][0]
-      query2 = @neo.execute_query("START n=node(*) MATCH n-[r]->() WHERE n.athena ='#{params[:name]}' RETURN collect(type(r));")["data"][0]
+    	query = @neo.execute_query("START n=node(*) WHERE n.athena ='#{params[:name]}' RETURN n.name, n.course, n.year, n.living_group, n.id, n.likes;")["data"][0]
+      #query2 = @neo.execute_query("START n=node(*) MATCH n-[r]->() WHERE n.athena ='#{params[:name]}' RETURN collect(type(r)), collect(r.id);")["data"][0]
+      query2 = @neo.execute_query("START n=node(*) MATCH n-[r]->() WHERE n.athena ='#{params[:name]}' RETURN type(r), r.id;")["data"]
     	name = query[0]
     	course = query[1]
     	year = query[2]
     	living_group = query[3]
-    	likes = query[4]
-    	connections = query2[0].uniq
+      unique_id = query[4]
+    	likes = query[5]
+    	#connections = query2[0].uniq
     	children = Array.new
-    	connections.each do |c|
-        unique_id = (0...50).map{ ('a'..'z').to_a[rand(26)] }.join
-    		t = {:name => c, :type => "category", :id => unique_id, :children => Array.new }
-    		children << t
-    	end
+    	#connections.each do |c|
+      #  #unique_id = (0...50).map{ ('a'..'z').to_a[rand(26)] }.join
+    	#	t = {:name => c, :type => "category", :id => unique_id, :children => Array.new }
+    	#	children << t
+    	#end
+      puts "-----------------------------------"
+      puts query2
+      connections = query2.uniq
+      connections.each do |c|
+        category_name = c[0]
+        category_id = c[1]
+        t = {:name => category_name, :type => "category", :id => category_id, :children => Array.new }
+        children << t
+      end
     	ret = {:details => {:name => name, :course => course, :year => year, :living_group => living_group, :likes => likes},
-             :graph   => {:name => params[:name], :type => params[:type], :id => params[:id], :children => children }
+             :graph   => {:name => params[:name], :type => params[:type], :id => unique_id, :children => children }
             }
     	render :json => ret.to_json
     elsif params[:type] == "category"
-      query = @neo.execute_query("START n=node(*) MATCH (n)-[:#{params[:name]}]-(x) WHERE n.athena ='#{params[:parent]}' RETURN collect(x.athena);")["data"][0]
-      connections = query[0]
+      query = @neo.execute_query("START n=node(*) MATCH (n)-[:#{params[:name]}]-(x) WHERE n.athena ='#{params[:parent]}' RETURN x.athena, x.id;")["data"]
+      connections = query
       children = Array.new
       connections.each do |c|
-        unique_id = (0...50).map{ ('a'..'z').to_a[rand(26)] }.join
-        t = {:name => c, :type => "person", :id => unique_id, :children => Array.new }
+        #unique_id = (0...50).map{ ('a'..'z').to_a[rand(26)] }.join
+        person_athena = c[0]
+        person_id = c[1]
+        t = {:name => person_athena, :type => "person", :id => person_id, :children => Array.new }
         children << t
       end
       ret = {:details => {},
