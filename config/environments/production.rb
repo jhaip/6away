4b9eaf38cb783d0b8943e8afff52aa21aac246d8
Jacob Haip <jhaip@mit.edu>
2013-01-29 00:13:13 -0500

    switching to 6away url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,8 @@
 Tutorial::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
-  SITE_URL = "http://stark-hamlet-8685.herokuapp.com"
+  #SITE_URL = "http://stark-hamlet-8685.herokuapp.com"
+  SITE_URL = "www.6away.us"
 
   # Code is not reloaded between requests
   config.cache_classes = true
